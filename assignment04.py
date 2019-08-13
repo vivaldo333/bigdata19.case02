f79d3647516c037507a4e3edb7405c56f698409d
Mykhailo Makukha <m.makukha@gmail.com>
2019-08-14 00:23:49 +0300

    Added Assignment 04

---
+++ assignment04.py
@@ -0,0 +1,13 @@
+"""
+Assignment 04
+=============
+
+The goal of this assignment is to ignore eye regions of interest (ROI) that are not placed within face ROI.
+
+The code you will write in this file will be similar to main.py code, but will include additional rectangles filtering.
+
+Run this code with
+
+    > invoke run assignment04.py
+"""
+
